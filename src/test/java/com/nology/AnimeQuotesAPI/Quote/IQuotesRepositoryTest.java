ba78d9057a74801c52a75c6ed3bb391839c1c25d
iKarans <68602829+iKarans@users.noreply.github.com>
2021-12-19 14:30:50 +0000

    used test instance annotation to use before all so dont have to repeat save and new test quote

--- src/test/java/com/nology/AnimeQuotesAPI/Quote/IQuotesRepositoryTest.java
+++ src/test/java/com/nology/AnimeQuotesAPI/Quote/IQuotesRepositoryTest.java
@@ -1,31 +1,36 @@
 package com.nology.AnimeQuotesAPI.Quote;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @DataJpaTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IQuotesRepositoryTest {
 
     private IQuotesRepository underTest;
+    private Quote quote;
 
     @Autowired
     public IQuotesRepositoryTest(IQuotesRepository underTest) {
         this.underTest = underTest;
     }
 
+    @BeforeAll
+    void setUp() {
+        quote = new Quote(1, "Eren", "Kill", "AOT", "Cruel World");
+        underTest.save(quote);
+    }
+
     @Test
     void findAllByCategory() {
-        Quote quote = new Quote(1, "Eren", "Kill", "AOT", "Cruel World");
-        underTest.save(quote);
-        List<Quote> expected = new ArrayList<>(Arrays.asList(quote));
         List<Quote> actual = underTest.findAllByCategory("Cruel World");
         assertThat(actual.get(0))
                 .usingRecursiveComparison()
@@ -34,9 +39,6 @@ class IQuotesRepositoryTest {
 
     @Test
     void findAllByAnimeName() {
-        Quote quote = new Quote(2, "Eren", "Kill", "AOT", "Cruel World");
-        underTest.save(quote);
-        Quote expected = quote;
         List<Quote> actual = underTest.findAllByAnimeName("AOT");
         assertThat(actual.get(0))
                 .usingRecursiveComparison()
@@ -45,9 +47,6 @@ class IQuotesRepositoryTest {
 
     @Test
     void findAllByCharacterName() {
-        Quote quote = new Quote(3, "Eren", "Kill", "AOT", "Cruel World");
-        underTest.save(quote);
-        Quote expected = quote;
         List<Quote> actual = underTest.findAllByCharacterName("Eren");
         assertThat(actual.get(0))
                 .usingRecursiveComparison()
